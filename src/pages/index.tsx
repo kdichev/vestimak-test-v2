c3326e4fef4c349c99aec9fb15f04dd7ffe70fb4
kdichev <konstantin.dichev@gmail.com>
2024-02-15 02:27:18 +0100

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,45 @@ import React from "react";
 import { Link } from "gatsby";
 import slugify from "@sindresorhus/slugify";
 import { fetchGraphQL } from "../components";
+import {
+  AppBar,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+} from "@mui/material";
+import { theme } from "../theme";
 
-const IndexPage = ({ serverData, data }) => {
+const IndexPage = ({ serverData }) => {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      {serverData.pages.map((i) => (
-        <Link to={`${slugify(i.category)}/${slugify(i.title)}`}>
-          {i.title} (гледания: {i.views})
-        </Link>
-      ))}
-    </div>
+    <ThemeProvider theme={theme}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <AppBar>
+          <Toolbar>ВЕСТИМАК</Toolbar>
+        </AppBar>
+        <Toolbar></Toolbar>
+        <CssBaseline />
+        <Container sx={{ pt: 1 }}>
+          <Button color="primary" variant="contained" sx={{ mb: 1 }}>
+            Test
+          </Button>
+          {serverData.pages.map((i) => {
+            return (
+              <Card sx={{ mb: 1 }}>
+                <CardContent>
+                  <Link to={`/${slugify(i.category)}/${slugify(i.title)}`}>
+                    {i.title} (гледания: {i.views})
+                  </Link>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+// A custom theme for this app
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[700],
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
